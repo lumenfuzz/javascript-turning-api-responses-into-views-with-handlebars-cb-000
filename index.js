192cb2d79a798c5fe2be9146b5aa1e8a1531f8ec
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-15 19:35:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   const src = document.getElementById("repository-template").innerHTML
